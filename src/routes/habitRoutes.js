98dece11833f28bc31fc38f2bd490c3444bca648
haeun-chae <gkdms1399@gmail.com>
2026-07-09 14:27:42 +0900

    fix: 습관 API 라우트 경로 수정

--- src/routes/habitRoutes.js
+++ src/routes/habitRoutes.js
@@ -2,9 +2,9 @@ import express from "express";
 import * as habitController from '../controllers/habitController.js';
 const router = express.Router();
 
-router.get("/studies/:studyId/habits", habitController.getHabits);
-router.post("/studies/:studyId/habits", habitController.createHabit)
-router.patch("/habits/:habitId", habitController.patchHabit)
-router.delete("/habits/:habitId", habitController.deleteHabit)
+router.get("/study/:studyId/habit", habitController.getHabits);
+router.post("/study/:studyId/habit", habitController.createHabit)
+router.patch("/habit/:habitId", habitController.patchHabit)
+router.delete("/habit/:habitId", habitController.deleteHabit)
 
 export default router;
