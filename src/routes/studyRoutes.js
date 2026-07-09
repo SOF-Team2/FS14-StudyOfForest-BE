52a5f049b89d9314a86920c9195440e47d5596db
youHazi <yih1508@naver.com>
2026-07-09 16:16:29 +0900

    feat: studyRoute.js에 하위 라우트 연결

--- src/routes/studyRoutes.js
+++ src/routes/studyRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
+import focusRoute from "./focusRoute.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ message: "Study Route" });
+  res.json({ message: "Study Route" });
 })
+
+router.use("/", focusRoute);
 
 export default router;
