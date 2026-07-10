92839ae88ab499b938b781505e83eefb9fb2cf27
윤용일 <yongyong1617@gmail.com>
2026-07-10 18:43:16 +0900

    chore: main과 merge 준비

--- src/services/habitService.js
+++ src/services/habitService.js
@@ -1,12 +1,4 @@
-import dotenv from 'dotenv';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
-import { assert } from 'superstruct';
-
-dotenv.config();
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+import prisma from "../lib/prisma.js";
 
 export const getHabits = async () => {
     return prisma.habit.findMany();
