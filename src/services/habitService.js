06fcef006fe87846e80427dd65d79b427f4c8464
haeun-chae <gkdms1399@gmail.com>
2026-07-13 09:30:06 +0900

    refactor: 공통 prisma 인스턴스 적용

--- src/services/habitService.js
+++ src/services/habitService.js
@@ -1,19 +1,7 @@
 import dotenv from "dotenv";
-import {
-  PrismaPg
-} from "@prisma/adapter-pg";
-import {
-  PrismaClient
-} from "@prisma/client";
+import prisma from "../lib/prisma.js";
 
 dotenv.config();
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL
-});
-const prisma = new PrismaClient({
-  adapter
-});
 
 const throwError = (status, message) => {
   const error = new Error(message);
